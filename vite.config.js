c40551c2e22ffa1e77b654255fe32cdabc2e332f
miskerbirhanucse <miskerbirhanu@gmail.com>
2026-08-06 12:00:24 +0300

    made a correction

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  base: '/duo_cafe/', // <--- Add your repository name here!
 })
+
+
